6936cc51b5921053c0689a3f62ec0de94920115c
Ivo e Kami <Ivo e Kami@DESKTOP-NIGKST4>
2023-05-23 16:29:04 -0300

    Adicionar arquivos de projeto.

---
+++ RoletaDaIndecisao/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace RoletaDaIndecisao.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ RoletaDaIndecisao/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace RoletaDaIndecisao.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
