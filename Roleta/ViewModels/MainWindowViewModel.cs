6f561ef5ac4c55f21e087c326eae48ff0e793c79
ImmortalLotus <jttbsb@gmail.com>
2023-05-27 22:14:01 -0300

    add : propriedade para girar a roleta

--- Roleta/ViewModels/MainWindowViewModel.cs
+++ Roleta/ViewModels/MainWindowViewModel.cs
@@ -3,5 +3,11 @@
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+
+        public string Spin{ get; set; }
+        public MainWindowViewModel()
+        {
+            Spin = "rotate(10deg)";
+        }
     }
 }
