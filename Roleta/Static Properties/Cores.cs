2752d4267d8049eb5cbc1f3870814942c1b2f123
ImmortalLotus <jttbsb@gmail.com>
2023-06-02 13:37:28 -0300

    Helper Color Class

---
+++ Roleta/Static Properties/Cores.cs
@@ -0,0 +1,47 @@
+﻿using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roleta.Static_Properties
+{
+    public static class Cores
+    {
+        public static Color[] coresPastel = new Color[]
+            {
+                    Color.Parse("#FFC4C4"), // Rosa claro
+                    Color.Parse("#FFD6B8"), // Laranja claro
+                    Color.Parse("#FFE8AC"), // Amarelo claro
+                    Color.Parse("#F3F5AA"), // Verde claro
+                    Color.Parse("#D7F5AD"), // Verde claro
+                    Color.Parse("#A8F5B0"), // Verde claro
+                    Color.Parse("#A4F2D6"), // Azul claro
+                    Color.Parse("#A0E1F6"), // Azul claro
+                    Color.Parse("#AAC9FF"), // Azul claro
+                    Color.Parse("#D4C9FF"), // Lilás claro
+                    Color.Parse("#F4C9FF"), // Lilás claro
+                    Color.Parse("#FFC9F2"), // Rosa claro
+                    Color.Parse("#FFDAC1"), // Pêssego claro
+                    Color.Parse("#FFF6B5"), // Amarelo claro
+                    Color.Parse("#E8F6B5"), // Verde claro
+                    Color.Parse("#BFF6B5"), // Verde claro
+                    Color.Parse("#B5F6CF"), // Turquesa claro
+                    Color.Parse("#B5EBF6"), // Azul claro
+                    Color.Parse("#C1D2FF"), // Azul claro
+                    Color.Parse("#E0C1FF"), // Lilás claro
+                    Color.Parse("#FDC1FF"), // Lilás claro
+                    Color.Parse("#FFC1ED"), // Rosa claro
+                    Color.Parse("#FFDED0"), // Pêssego claro
+                    Color.Parse("#FFFADE"), // Amarelo claro
+                    Color.Parse("#E4FADE"), // Verde claro
+                    Color.Parse("#C1FADE"), // Verde claro
+                    Color.Parse("#B5FAD9"), // Turquesa claro
+                    Color.Parse("#B5F1FA"), // Azul claro
+                    Color.Parse("#C4DAFF"), // Azul claro
+                    Color.Parse("#ECC4FF"), // Lilás claro
+                    Color.Parse("#FFC4FF")  // Rosa claro
+            };
+    }
+}
